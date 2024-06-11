4159ee3b0f575a028854109e18ad5ab2cbd9c911
조예설 <choyeseol07@gmail.com>
2024-06-11 18:17:11 +0900

    init

---
+++ src/main/java/com/hash/harp/HarpApplication.java
@@ -0,0 +1,13 @@
+package com.hash.harp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HarpApplication.class, args);
+	}
+
+}
